63845ddd3755c615d53187c476da04363efcd2c9
vitorhcl <vitorhcl00@gmail.com>
2023-02-27 08:18:32 -0300

    Nome nullable

--- Models/Personagem.cs
+++ Models/Personagem.cs
@@ -5,7 +5,7 @@ namespace RpgApi.Models
     public class Personagem
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int PontosVida { get; set; }
         public int Forca { get; set; }
         public int Defesa { get; set; }
